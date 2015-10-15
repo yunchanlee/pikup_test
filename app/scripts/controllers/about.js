89316867feafcb8aa90cc70a07b935ae2a4c0ab9
yclee0210 <yclee1987@gmail.com>
2015-10-15 11:19:51 +0900

    test commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pikupApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the pikupApp
+ */
+angular.module('pikupApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pikupApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the pikupApp
+ */
+angular.module('pikupApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
